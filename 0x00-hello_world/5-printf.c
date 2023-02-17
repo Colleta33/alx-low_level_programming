7826f02b4ced8beb60ac3f488ec2c0bb7d9f0a38
annacolleta <annacolletamtetwa@gmail.com>
2023-02-16 19:38:07 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Aprogram that print aline using printf
+ * retun: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
