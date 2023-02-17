1d7d699dd9ed3a67d0547e0efa2fd9a884260385
annacolleta <annacolletamtetwa@gmail.com>
2023-02-16 20:00:50 -0800

    Assembler

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - A program that print different values
+ * return: 0 (success)
+ */int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float;
+	printf("size of a char: %lu bytes")
+	return (0);
