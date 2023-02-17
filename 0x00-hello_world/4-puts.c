b940a32eead8ab232ebfa57eea040cc72eb7d312
annacolleta <annacolletamtetwa@gmail.com>
2023-02-16 19:16:35 -0800

    put c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - Program that print a line with puts function
- * Return Always 0 (success)
+ * main - A program that print a line with puts function
+ * Return: 0 (success)
  */
 int main(void)
 {
