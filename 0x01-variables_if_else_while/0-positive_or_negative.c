1bfe580346ae66b0dbd814820c5635595aca7d9a
annacolleta <annacolletamtetwa@gmail.com>
2023-02-17 05:57:12 -0800

    main fumction

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,15 @@
+#inlude <stdio.h>
+/**
+ *
+ * main - Main function
+ *
+ * Return: success 0
+ */
+int main(void)
+{
+int i:
+for (i=0; i<10; itt)
+	printf("%d". i):
+		printf("\n");
+return (0);
+
