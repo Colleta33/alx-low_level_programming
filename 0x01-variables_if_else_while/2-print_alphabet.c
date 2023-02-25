cc4517ef55543447bcf0b1125c0e941e2d319259
annacolleta <annacolletamtetwa@gmail.com>
2023-02-25 02:46:58 -0800

    lower case alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
-
 /**
- * main - ptint alpha in lower case
+ * main - Entry point
+ * Description: 'Print alphabet in lowercase'
  * Return: Always 0
  */
 int main(void)
 {
-	char lc;
+	int n = 97;
 
-	for (lc = 'a'; lc <= 'z'; lc++);
+	while (n <= 122)
 	{
-		putchar(lc);
+		putchar(n);
+		n++;
 	}
 	putchar('\n');
 	return (0);
 }
-
