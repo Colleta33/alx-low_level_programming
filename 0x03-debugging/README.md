4cb0973d5aab875ca2c3552a8dbd4793b6e66cff
annacolleta <annacolletamtetwa@gmail.com>
2023-02-25 05:30:21 -0800

    printing alphabet in reverse

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+A test for a postitive_or_negative() function
---
+++ 0x03-debugging/postitive_or_negative.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* postitive_or_negative - check for positive or negative number
+*
+* @i: the number to be checked
+*
+* Return: 0
+*/
+
+void postitive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i)
+	else
+		printf("%d is positive\n", i)
+			return (0);
+}
