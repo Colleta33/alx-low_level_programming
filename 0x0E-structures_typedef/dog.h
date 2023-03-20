6e8f77b35b7a02ecf6cf2e0f5ddef69226f98186
annacolleta <annacolletamtetwa@gmail.com>
2023-03-20 06:30:45 -0700

    poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,31 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+*struct dog - a dog's info
+*@name: first member
+*@age: second memmber
+*@owner: third member
+*
+* Desription: longer description
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+
+#endif
