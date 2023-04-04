2e7beadf188a4c7ccc57e6e0ce17e63013291d26
annacolleta <annacolletamtetwa@gmail.com>
2023-04-04 05:53:18 -0700

    add node

--- 0x13-more_singly_linked_lists/2-main.c
+++ 0x13-more_singly_linked_lists/2-main.c
@@ -13,7 +13,7 @@ int main(void)
     listint_t *head;
 
     head = NULL;
-    add_nodeint(&head, 0);
+    add_nodeint_end(&head, 0);
     add_nodeint(&head, 1);
     add_nodeint(&head, 2);
     add_nodeint(&head, 3);
