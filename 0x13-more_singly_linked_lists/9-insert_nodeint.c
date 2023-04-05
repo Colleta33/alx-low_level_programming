d52e32aedbc5935cade66570dc77a013f51544f7
annacolleta <annacolletamtetwa@gmail.com>
2023-04-05 04:43:02 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,52 +4,47 @@
 
 /**
  *insert_nodeint_at_index -  inserts a new node at a given position.
- *@head: pointer that point the node
- *@idx:index of the list
- *@n: number of nodes
- *Return: the address of the new node
- *NULL if it failed
- *
+ *@head: pointer to the node
+ *@idx: index to the node
+ *@n: integer
+ *Return: address of the new node or  NULL (failed)
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current;
-	unsigned int idx;
+	listint_t *new_node;
+	listint_t *temp;
+	unsigned int i;
 
-	if (head == NULL)
+	if (head  == NULL)
 		return (NULL);
 
-	*new_node = malloc(sizeof(listint_t));
+	new_node = malloc(sizeof(listint_t));
 
 	if (new_node == NULL)
 		return (NULL);
-
 	new_node->n = n;
-	current = *head;
+	new_node->next = NULL;
 
 	if (idx == 0)
 	{
-		new_node->next = current;
+		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
+	temp = *head;
 
-	*current_node = head;
-	unsigned int current_idx = 0;
-
-	while (current_node != NULL && current_idx < idx - 1)
+	for (i = 0; i < (idx -1); i++)
 	{
-
-		current_node = current_node->next;
-		current_idx++;
+		if (temp == NULL)
+		{
+			free(new_node);
+			return (NULL);
+		}
+		temp = temp->next;
 	}
+	new_node->next = temp->next;
+	temp->next = new_node;
 
-	if (current_node == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next = current_node->next;
-	current_node->next = new_node;
 	return (new_node);
 }
+
