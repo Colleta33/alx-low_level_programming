913652ad81fb97b745ea835f3131b82101379b76
annacolleta <annacolletamtetwa@gmail.com>
2023-04-05 18:53:40 -0700

    update

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -11,13 +11,6 @@
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
-	const listint_t *current = *head;
-
-		while (current != NULL)
-		{
-			printf("%d\n", current->n);
-			current = current->next;
-		}
 
 	new_node = malloc(sizeof(listint_t));
 
