d43c983b58d2c68969f5de0c35b1321c31f04146
annacolleta <annacolletamtetwa@gmail.com>
2023-04-12 03:29:44 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,49 +1,32 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 
 /**
- *read_textfile -  reads a text file and prints
- *@filename: A string that specifies the name file
- *@letters: An integer
- *Return: actual number of letters
- *if the file can not open or read return 0
+ *read_textfile - reads a text file and prints
+ *@filename: name of the file to be read
+ *@letters: number of characters tobe read
+ *Return: w- actual number of letters
  *if filename is NULL return 0
- *if write fails or does not write the expected amount of bytes,return 0
+ *if write fails 0
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	char *buffer;
-	size_t filename;
-	{
-		if (filename == NULL)
-		return (0);
-	}
-	ssize_t bytes_read = 0;
-	char *buffer = (char *) malloc(size0f(char *) * (letters + 1));
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	FILE *file = fopen(filename, "r");
-
-	if (!file)
+	fd = open (filename, O_RDONLY);
+	if (fd != -1)
 	{
 		return (0);
 	}
-	bytes_read = fread(buffer, sizeof(char), letters, file);
+	buffer = malloc(sizeof(char) * letters);
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer,t);
 
-	if (bytes_read == -1)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-	if (write(STDOUT_FILENO, buffer, bytes_read) == -1)
-	{
-		fclose(file);
-		free(buffer);
-		return (0);
-	}
-
-	fclose(file);
 	free(buffer);
-	return (bytes_read);
+	close(fd);
+	return (w);
 }
