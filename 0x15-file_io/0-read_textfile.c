0e44d9f4af651f3e10224e69445083af947855b9
annacolleta <annacolletamtetwa@gmail.com>
2023-04-12 03:50:49 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
-	if (fd = -1)
+	if (fd == -1)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
