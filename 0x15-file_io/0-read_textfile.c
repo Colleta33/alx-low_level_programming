5d5d92ee8a9e444ccdd10a0d7435f872d17fec39
annacolleta <annacolletamtetwa@gmail.com>
2023-05-08 12:44:55 -0700

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,43 +6,41 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	FILE *fp;
 	char *buf;
-    size_t n;
-    ssize_t w;
+	size_t n;
+	ssize_t w;
 
-    if (filename == NULL)
-        return 0;
+	if (filename == NULL)
+		return (0);
+	fp = fopen(filename, "r");
 
-    fp = fopen(filename, "r");
-    if (fp == NULL)
-        return 0;
+	if (fp == NULL)
+		return (0);
 
-    buf = malloc(sizeof(char) * (letters + 1));
-    if (buf == NULL)
-    {
-        fclose(fp);
-        return 0;
-    }
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+	{
+		fclose(fp);
+		return (0);
+	}
+	n = fread(buf, sizeof(char), letters, fp);
+	if (n != letters && ferror(fp))
+	{
+		perror("fread");
+		free(buf);
+		fclose(fp);
+		return (0);
+	}
+	buf[n] = '\0';
 
-    n = fread(buf, sizeof(char), letters, fp);
-    if(n != letters && ferror(fp))
-    {
-	    perror("fread");
-	    free(buf);
-	    fclose(fp);
-	    return (0);
-    }
-    buf[n] = '\0';
-
-    w = fwrite(buf, sizeof(char), n, stdout);
-    if(w != (ssize_t)n && ferror (stdout))
-    {
-	    perror("fwrite");
-	    free(buf);
-	    fclose(fp);
-	    return (0);
-    }
-    free (buf);
-    fclose(fp);
-
-    return (w);
+	w = fwrite(buf, sizeof(char), n, stdout);
+	if (w != (ssize_t)n && ferror(stdout))
+	{
+		perror("fwrite");
+		free(buf);
+		fclose(fp);
+		return (0);
+	}
+	free(buf);
+	fclose(fp);
+	return (w);
 }
