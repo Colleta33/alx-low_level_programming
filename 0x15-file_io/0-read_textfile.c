3d697a956b4ca678ad11ac769c117f18a0ea66f7
annacolleta <annacolletamtetwa@gmail.com>
2023-05-08 04:45:42 -0700

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp;
+	char *buf;
+    size_t n;
+    ssize_t w;
+
+    if (filename == NULL)
+        return 0;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL)
+        return 0;
+
+    buf = malloc(sizeof(char) * (letters + 1));
+    if (buf == NULL)
+    {
+        fclose(fp);
+        return 0;
+    }
+
+    n = fread(buf, sizeof(char), letters, fp);
+    if(n != letters && ferror(fp))
+    {
+	    perror("fread");
+	    free(buf);
+	    fclose(fp);
+	    return (0);
+    }
+    buf[n] = '\0';
+
+    w = fwrite(buf, sizeof(char), n, stdout);
+    if(w != (ssize_t)n && ferror (stdout))
+    {
+	    perror("fwrite");
+	    free(buf);
+	    fclose(fp);
+	    return (0);
+    }
+    free (buf);
+    fclose(fp);
+
+    return (w);
+}
