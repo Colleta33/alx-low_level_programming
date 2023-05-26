c36e0968d3c0a6931823d127ee0018b2d2920cee
annacolleta <annacolletamtetwa@gmail.com>
2023-05-26 16:53:10 -0700

    bit manipulation

--- 0x14-bit_manipulation/crackme3.c
+++ 0x14-bit_manipulation/crackme3.c
@@ -3,20 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-    if (argc < 2)
-    {
-	    printf("Usage: %s password", argv[0]);
-	    return 1;
-    }
+	if (argc < 2)
+	{
+		printf("Usage: %s password", argv[0]);
+		return (1);
+	}
+	char *password = "Hol";
 
-    char *password = "Hol";
-    if (strcmp(argv[1], password) == 0)
-    {
-        printf("Congratulations!\n");
-        return 0;
-    } else 
-    {
-        printf("Incorrect passwordi\n");
-        return 1;
-    }
+	if (strcmp(argv[1], password) == 0)
+	{
+		printf("Congratulations!\n");
+		return (0);
+	}
+	else
+	{
+		printf("Incorrect passwordi\n");
+		return (1);
+	}
 }
