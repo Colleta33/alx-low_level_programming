a04e303d0ce9efa7cf87d1d7264dd13c3230c8f7
annacolleta <annacolletamtetwa@gmail.com>
2023-06-21 00:28:25 -0700

    dynamic libraries updates

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,7 +1,11 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * _puts - Entry point
  *@str: string pointer
  * Return: Always 0 (Success)
  */
-void _puts(char *str);
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
